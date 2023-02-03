615e30f208a5bc94574a6b62c5ff30f9bd0c4f5b
Morning <morning@admins-iMac.local>
2023-02-03 11:37:09 +0800

    2/3

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,7 +4,8 @@ import Page from "./Page";
 import PageButton from "./subcomp/PageButton";
 
 export default function Pagination() {
-  const [currentPage, setCurrentPage] = useState("1");
+  const maxNum = 5000;
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className="pagination">
       <Link
@@ -16,22 +17,69 @@ export default function Pagination() {
         <PageButton btnName={"Pre"} btnClass={"inactive"} />
       </Link>
       <Link
-        to={`/page/${currentPage}`}
+        to={`/page/${1}`}
+        onClick={() => {
+          setCurrentPage(1);
+        }}
+      >
+        <PageButton btnName={"1"} btnClass={"inactive"} />
+      </Link>
+      <div className="dots">...</div>
+
+      <Link
+        to={`/page/${currentPage - 2}`}
+        onClick={() => {
+          setCurrentPage(currentPage - 2);
+        }}
+      >
+        <PageButton btnName={currentPage - 2} btnClass={"inactive"} />
+      </Link>
+
+      <Link
+        to={`/page/${currentPage - 1}`}
         onClick={() => {
           setCurrentPage(currentPage - 1);
         }}
       >
-        <PageButton btnName={"Pre"} btnClass={"inactive"} />
+        <PageButton btnName={currentPage - 1} btnClass={"inactive"} />
       </Link>
+
+      <PageButton btnName={currentPage} btnClass={"active"} />
+
       <Link
         to={`/page/${currentPage + 1}`}
         onClick={() => {
           setCurrentPage(currentPage + 1);
         }}
       >
-        <PageButton btnName={"Next"} btnClass={"inactive"} />
+        <PageButton btnName={currentPage + 1} btnClass={"inactive"} />
+      </Link>
+      <Link
+        to={`/page/${currentPage + 2}`}
+        onClick={() => {
+          setCurrentPage(currentPage + 2);
+        }}
+      >
+        <PageButton btnName={currentPage + 2} btnClass={"inactive"} />
       </Link>
 
+      <div className="dots">...</div>
+      <Link
+        to={`/page/${maxNum}`}
+        onClick={() => {
+          setCurrentPage(maxNum);
+        }}
+      >
+        <PageButton btnName={maxNum} btnClass={"inactive"} />
+      </Link>
+      <Link
+        to={`/page/${currentPage + 1}`}
+        onClick={() => {
+          setCurrentPage(Number(currentPage + 1));
+        }}
+      >
+        <PageButton btnName={"Next"} btnClass={"inactive"} />
+      </Link>
       <Page />
     </div>
   );
